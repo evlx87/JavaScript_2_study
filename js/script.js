8672f7bc428e10a4d3924affd935ff02da9d5295
evlx87 <evlx@mail.ru>
2021-03-08 00:49:00 +0300

    Add totalAmount, empty cart features and small code changes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,83 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-];
+class GoodsList {
+    constructor(container = '.goods-list') {
+        this.container = container;
+        this.goods = [];
+        this.allGoods = []; //массив товаров с версткой
+        this.fetchGoods();
+        this.render(); //вывод товаров на страницу
+    }
 
-const $goodsList = document.querySelector('.goods-list');
-  
-const renderGoodsItem = ({ title, price }) => {
-    return `<div class="goods-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="btn-buy">Купить</button>
-            </div>`;
-};
-  
-const renderGoodsList = (list = goods) => {
-    let goodsList = list.map(
-            item => renderGoodsItem(item)
-        ).join('\n');
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ];
+    }
 
-    $goodsList.insertAdjacentHTML('beforeend', goodsList);
+    render() {
+        const block = document.querySelector(this.container);
+        for (let good of this.goods) {
+            const item = new GoodsItem(good);
+            this.allGoods.push(item);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+
+    totalAmount() {
+        let sum = 0;
+        this.goods.forEach((good) => {
+            sum += good.price;
+        });
+        document.querySelector('.total-amount').innerHTML = `Суммарная стоимость всех товаров: ${sum}`;
+    }
 }
-  
-renderGoodsList();
+
+
+class GoodsItem {
+    constructor(good, img = 'img/200x150.png') {
+        this.title = good.title;
+        this.price = good.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="goods-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="btn-buy">Купить</button>
+            </div>`
+    }
+}
+
+class CartList {
+    constructor() {
+
+    }
+    // метод для очистки корзины
+    clearAll() {
+
+    }
+    //метод подсчета суммы купленных товаров
+    totalSum() {
+
+    }
+    //метод оплаты товаров
+    pay() {
+
+    }
+}
+
+class CartItem {
+    constructor() {
+
+    }
+    //удаляет один элемент из корзины
+    deleteItem() {
+
+    }
+}
+
+let list = new GoodsList();
+list.totalAmount();
