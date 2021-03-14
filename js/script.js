c7d93644530b2d49415e6f867514d96f3c28cedc
evlx87 <evlx@mail.ru>
2021-03-14 19:16:10 +0300

    Add get catalog from api(url)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
+const API = 'https://raw.githubusercontent.com/evlx87/JavaScript_2_study/hw_less_3/json';
+
 class GoodsList {
     constructor(container = '.goods-list') {
         this.container = container;
         this.goods = [];
-        this.allGoods = []; //массив товаров с версткой
-        this.fetchGoods();
-        this.render(); //вывод товаров на страницу
+        this.allGoods = [];
+        this._getGoods()
+            .then(data => {
+                this.goods = [...data];
+                this.render()
+            });
     }
 
-    fetchGoods() {
-        this.goods = [
-            { title: 'Shirt', price: 150 },
-            { title: 'Socks', price: 50 },
-            { title: 'Jacket', price: 350 },
-            { title: 'Shoes', price: 250 },
-        ];
+    _getGoods() {
+        return fetch(`${API}/catalogData.json`)
+        .then(result => result.json())
+        .catch(error => {
+            console.log(error);
+        })  
     }
 
     render() {
@@ -26,35 +30,69 @@ class GoodsList {
     }
 
     totalAmount() {
-        let sum = 0;
-        this.goods.forEach((good) => {
-            sum += good.price;
-        });
-        document.querySelector('.total-amount').innerHTML = `Суммарная стоимость всех товаров: ${sum}`;
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
     }
 }
 
-
 class GoodsItem {
     constructor(good, img = 'img/200x150.png') {
-        this.title = good.title;
+        this.title = good.product_name;
+        this.id = good.id_product;
         this.price = good.price;
         this.img = img;
     }
+
     render() {
-        return `<div class="goods-item">
-                <img src="${this.img}">
-                <h3>${this.title}</h3>
-                <p>${this.price}</p>
-                <button class="btn-buy">Купить</button>
-            </div>`
+        return `<div class="goods-item" data-id="${this.id}">
+                    <img src="${this.img}" alt="Изображение товара">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} руб.</p>
+                    <button class="btn-buy">Купить</button>
+                </div>`
     }
 }
 
 class CartList {
     constructor() {
-
+        this.countGoods = 0; //Общая стоимость товаров
+        this.amount = 0; //Общая стоимость товаров
+        this.basketItems = []; //Массив для хранения товаров
+        this.container = '.cart-content'; // контейнер для корзины
+        this.allGoods = [];
+        this._getBasket()
+            .then(() => {
+                this.render();
+            });
     }
+
+    //метод получения товаров в корзине
+    _getBasket() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .then(data => {
+                this.basketItems = data;
+                this.countGoods = data.countGoods;
+                this.totalPrice = data.totalPrice;
+                this.amount = data.amount;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    //отрисовка корзины
+    render() {
+        document.querySelector(this.container).innerHTML = '';
+        const block = document.querySelector(this.container);
+        for (let good of this.basketItems.contents) {
+            const prod = new CartItem(good);
+            this.allGoods.push(prod);
+            block.insertAdjacentHTML('beforeend', prod.render());
+        }
+        let out = `<div class="out">Всего товаров в корзине: ${this.countGoods }<br> На сумму: ${this.amount} рублей</div> `;
+        block.insertAdjacentHTML('beforeend', out);
+    }
+
     // метод для очистки корзины
     clearAll() {
 
@@ -67,11 +105,31 @@ class CartList {
     pay() {
 
     }
+
+    //изменить товары в корзине
+    changeGoods() {
+
+    }
 }
 
 class CartItem {
-    constructor() {
-
+    constructor(product) {
+        this.title = product.product_name;
+        this.id = product.id_product;
+        this.amount = product.amount;
+        this.price = product.price;
+        this.quantity = product.quantity;
+    }
+    
+    render() {
+        return `<div class="cart-item">
+                    <div class="cart-desc">
+                        <h3 class="cart-name">${ this.title }</h3>
+                        <p>Количество: ${this.quantity} шт.</p>
+                        <p class="cart-price">Цена: ${this.price } руб.</><br>
+                        <p class="cart-price">Итого: ${this.price} руб.</p><br>
+                  </div>
+              </div>`
     }
     //удаляет один элемент из корзины
     deleteItem() {
@@ -81,3 +139,5 @@ class CartItem {
 
 let list = new GoodsList();
 list.totalAmount();
+list.render();
+let cart = new CartList();
