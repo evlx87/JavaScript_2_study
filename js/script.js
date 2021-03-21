a5966b73ca00cde1fe8151bd78aab7453e03e96b
evlx87 <evlx@mail.ru>
2021-03-21 16:13:43 +0300

    add hw_less_5

--- js/script.js
+++ js/script.js
@@ -1,143 +1,105 @@
-const API = 'https://raw.githubusercontent.com/evlx87/JavaScript_2_study/hw_less_3/json';
+const API = 'https://raw.githubusercontent.com/evlx87/JavaScript_2_study/hw_less_5/json';
 
-class GoodsList {
-    constructor(container = '.goods-list') {
-        this.container = container;
-        this.goods = [];
-        this.allGoods = [];
-        this._getGoods()
-            .then(data => {
-                this.goods = [...data];
-                this.render()
-            });
-    }
+const app = new Vue({
+    el: '#app',
+    data: {
+        isVisibleCart: false,
+        searchLine: '',
+        catalogUrl: '/catalogData.json',
+        products: [],
+        cartItems: [],
+        isCartFull: false,
+        cartProductsCount: 0,
+        cartProductsSum: 0,
+        imgCatalog: 'https://placehold.it/200x150',
+        imgCart: `https://placehold.it/50x70`
+    },
+    computed: {},
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product) {
+            console.log(product.id_product);
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if (data.result) {
+                        let find = this.cartItems.find(el => el.id_product === product.id_product);
+                        this.cartProductsCount++;
+                        this.cartProductsSum += product.price;
+                        if (find) {
+                            find.quantity++;
+                        } else {
+                            let prod = Object.assign({
+                                quantity: 1
+                            }, product);
+                            this.cartItems.push(prod);
+                        }
+                        this.isCartFull = !!this.cartItems.length;
 
-    _getGoods() {
-        return fetch(`${API}/catalogData.json`)
-        .then(result => result.json())
-        .catch(error => {
-            console.log(error);
-        })  
-    }
-
-    render() {
-        const block = document.querySelector(this.container);
-        for (let good of this.goods) {
-            const item = new GoodsItem(good);
-            this.allGoods.push(item);
-            block.insertAdjacentHTML("beforeend", item.render());
-        }
-    }
-
-    totalAmount() {
-        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
-    }
-}
-
-class GoodsItem {
-    constructor(good, img = 'img/200x150.png') {
-        this.title = good.product_name;
-        this.id = good.id_product;
-        this.price = good.price;
-        this.img = img;
-    }
-
-    render() {
-        return `<div class="goods-item" data-id="${this.id}">
-                    <img src="${this.img}" alt="Изображение товара">
-                    <h3>${this.title}</h3>
-                    <p>${this.price} руб.</p>
-                    <button class="btn-buy">Купить</button>
-                </div>`
-    }
-}
-
-class CartList {
-    constructor() {
-        this.countGoods = 0; //Общая стоимость товаров
-        this.amount = 0; //Общая стоимость товаров
-        this.basketItems = []; //Массив для хранения товаров
-        this.container = '.cart-content'; // контейнер для корзины
-        this.allGoods = [];
-        this._getBasket()
-            .then(() => {
-                this.render();
-            });
-    }
-
-    //метод получения товаров в корзине
-    _getBasket() {
-        return fetch(`${API}/getBasket.json`)
-            .then(result => result.json())
-            .then(data => {
-                this.basketItems = data;
-                this.countGoods = data.countGoods;
-                this.totalPrice = data.totalPrice;
-                this.amount = data.amount;
+                        console.log(this.cartProductsCount, this.cartProductsSum);
+                    } else {
+                        console.log('Error!');
+                    }
+                })
+        },
+        removeProduct(product) {
+            this.getJson(`${API}/deleteFromBasket.json`)
+                .then(data => {
+                    if (data.result) {
+                        let id = +product.id_product;
+                        let find = this.cartItems.find(el => el.id_product === id);
+                        this.cartProductsCount--;
+                        this.cartProductsSum -= product.price;
+                        if (find.quantity > 1) {
+                            find.quantity--;
+                        } else {
+                            this.cartItems.splice(this.cartItems.indexOf(find), 1);
+                        }
+                        this.isCartFull = !!this.cartItems.length;
+                    } else {
+                        console.log('Error!');
+                    }
+                })
+        },
+        FilterGoods(value) {
+            const regexp = new RegExp(value, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+            this.products.forEach(el => {
+                const block = document.querySelector(`.product-item[data-id="${el.id_product}"]`);
+                if (this.filtered.includes(el)) {
+                    block.classList.remove('invisible')
+                } else {
+                    block.classList.add('invisible')
+                }
             })
-            .catch(error => {
-                console.log(error);
+        },
+        init() {
+            document.querySelector('.search-form').addEventListener('submit', e => {
+                e.preventDefault();
+            });
+            if (this.cartItems.length) {
+                this.isCartFull = true;
+            }
+        },
+    },
+    mounted() {
+        this.init();
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
             })
     }
-
-    //отрисовка корзины
-    render() {
-        document.querySelector(this.container).innerHTML = '';
-        const block = document.querySelector(this.container);
-        for (let good of this.basketItems.contents) {
-            const prod = new CartItem(good);
-            this.allGoods.push(prod);
-            block.insertAdjacentHTML('beforeend', prod.render());
-        }
-        let out = `<div class="out">Всего товаров в корзине: ${this.countGoods }<br> На сумму: ${this.amount} рублей</div> `;
-        block.insertAdjacentHTML('beforeend', out);
-    }
-
-    // метод для очистки корзины
-    clearAll() {
-
-    }
-    //метод подсчета суммы купленных товаров
-    totalSum() {
-
-    }
-    //метод оплаты товаров
-    pay() {
-
-    }
-
-    //изменить товары в корзине
-    changeGoods() {
-
-    }
-}
-
-class CartItem {
-    constructor(product) {
-        this.title = product.product_name;
-        this.id = product.id_product;
-        this.amount = product.amount;
-        this.price = product.price;
-        this.quantity = product.quantity;
-    }
-    
-    render() {
-        return `<div class="cart-item">
-                    <div class="cart-desc">
-                        <h3 class="cart-name">${ this.title }</h3>
-                        <p>Количество: ${this.quantity} шт.</p>
-                        <p class="cart-price">Цена: ${this.price } руб.</><br>
-                        <p class="cart-price">Итого: ${this.price} руб.</p><br>
-                  </div>
-              </div>`
-    }
-    //удаляет один элемент из корзины
-    deleteItem() {
-
-    }
-}
-
-let list = new GoodsList();
-list.totalAmount();
-list.render();
-let cart = new CartList();
+});
